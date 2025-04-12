d1456c5a615fd08df0e03bbae1db21652b110166
Olya <ponomaryova.olya.y@gmail.com>
2025-04-12 16:00:42 +0200

    feat: add project skeleton; delete unused svg and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div className="text-2xl">Hello world</div>;
+  return (
+    <>
+      <header className="text-2xl">here goes header: navigation + logo</header>
+      <section>img from insta on full screen </section>
+      <section>
+        header products in stock. paragrap what products are represented. sanity
+        + cloudinary
+      </section>
+      <section className="text-2xl">
+        two columns text about brand + picture hands and sewing machine
+      </section>
+      <footer className="text-2xl">
+        logo home about shop. shipping FAQ as pages. Lets connect Instagram icon
+        Facebook icon This website was designed and developed by Olya Po
+      </footer>
+    </>
+  );
 }
