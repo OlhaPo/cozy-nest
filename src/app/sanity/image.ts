c760bba2dae0aa11f3957a092c2e6bd0488d6096
Olya <ponomaryova.olya.y@gmail.com>
2025-04-23 19:03:55 +0200

    fix: source type in sanity image

--- src/app/sanity/image.ts
+++ src/app/sanity/image.ts
@@ -1,10 +1,10 @@
 import imageUrlBuilder from '@sanity/image-url';
-import type { ImageSource } from '@sanity/image-url/lib/types/types';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 import { client } from './client';
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: ImageSource) {
+export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
