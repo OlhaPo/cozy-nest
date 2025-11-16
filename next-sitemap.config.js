d6a8e01cf92aa4d7b0cd0d492428fd4b41ff4b6d
Olya <ponomaryova.olya.y@gmail.com>
2025-11-16 14:34:13 +0100

    chore: add valid links to SM; generate robots.txt and sitemap.xml files

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.kocienest.pl/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
--- src/components/common/Footer/contants.ts
+++ src/components/common/Footer/contants.ts
@@ -1,5 +1,6 @@
-export const LINKEDIN_LINK = 'https://www.linkedin.com/in/olha-ponomarova/';
-export const INSTAGRAM_LINK = 'https://www.instagram.com/cozy_nest.home/';
-export const FB_LINK = 'https://www.facebook.com/share/1A6AKsx46x/';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/olga-ponomarova/';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/kocie_nest.handmade/';
+export const FB_LINK =
+  'https://www.facebook.com/share/14R5v23ZS6C/?mibextid=wwXIfr';
 export const FB_MESSENGER = 'https://m.me/1QoXfUZ7Rm/?mibextid=wwXIfr';
 export const EMAIL = 'kocienest.handmade@gmail.com';
